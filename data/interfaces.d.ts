ac96a297d3e030d0efdb910de8581b177764ea1e
Kitson Kelly <me@kitsonkelly.com>
2017-12-15 11:47:53 +0000

    Add interface to packages.json

---
+++ data/interfaces.d.ts
@@ -0,0 +1,29 @@
+/**
+ * Interfaces and types common to dojo/meta
+ */
+export namespace Meta {
+	/**
+	 * The interface describing the `/data/packages.json`
+	 */
+	interface PackagesJson {
+		/**
+		 * A package that is part of Dojo 2 repo, where the key is the npm package name
+		 */
+		[package: string]: {
+			/**
+			 * A flag that indicates that package is _deprecated_ and therefore no longer being maintained
+			 */
+			deprectated?: boolean;
+
+			/**
+			 * The organisation and repository name on GitHub
+			 */
+			github: string;
+
+			/**
+			 * A flag that indicates that the package is not published on npm
+			 */
+			nopublish?: boolean;
+		}
+	}
+}
